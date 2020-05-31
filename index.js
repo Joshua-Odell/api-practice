609722887ad516f3b2782eb296e4e3b91261b1a8
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-05-31 14:06:17 -0500

    syntax fix

--- index.js
+++ index.js
@@ -4,27 +4,32 @@ let number = 3;
 let pictures = [];
 let result = "";
 
+
 function pull(){
-    number = $("#number").val();
+    number = parseInt($("#number").val());
     if (number>50 || number<1) {
         alert("please choose a number between 1 and 50");
     }
     else {
-        fetch("https://dog.ceo/api/breeds/image/random/" + number)
-        .then(response => response.json())
-        .then(responseJson => pictures = responseJson.message)
-        .then(response =>
-        display());
+        fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
+            .then(response => response.json())
+            .then(response => display(response.message))
+            .catch(error => renderError(error))
     }
-    
 }
 
-function display() {
-    for (let i = 0; i<pictures.length; i++){
-        let temp = pictures[i];
-        let pic = '<img src="' + temp + '">'
-        result = result.concat(pic);
-    }    
+function renderError(error) {
+    console.log({error})
+    $('.result').html(error);
+}
+
+function display(pictures) {
+     let result = ''
+     for (let i = 0; i<pictures.length; i++){
+         let temp = pictures[i];
+         let pic = '<img src="' + temp + '">'
+         result = result.concat(pic);
+     }
     $('.result').html(result);
     
 }
